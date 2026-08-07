a4fdcc84e56c9baa1678246e360b1f24094377a3
wfptn <wfptny@gmail.com>
2026-08-08 01:51:08 +0200

    Fix production login page styling

--- src/main/java/com/example/onlinejava/SecurityConfig.java
+++ src/main/java/com/example/onlinejava/SecurityConfig.java
@@ -19,6 +19,7 @@ public class SecurityConfig {
                         .requestMatchers(
                                 "/",
                                 "/index.html",
+                                "/css/login.css",
                                 "/oauth2/**",
                                 "/login/**"
                         )
